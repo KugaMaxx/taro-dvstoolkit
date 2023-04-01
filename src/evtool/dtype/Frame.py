a9a818a7bc87b85708344c3caff6fad7fc4d364b
KugaMaxx <KugaMaxx@outlook.com>
2023-04-01 20:51:53 +0800

    Add `DvsFile.Save()` --- * src/evtool/dtype/Frame.py: modify `find_closest()`     + line   21 to   21: return idx rather than [timestamp, image]
    * src/evtool/dvs/DvsFile.py: hello.py: a function to save mat
        + line   84 to   98: add `to_mat()`

--- src/evtool/dtype/Frame.py
+++ src/evtool/dtype/Frame.py
@@ -4,7 +4,7 @@ from ._package import *
 class Frame(np.recarray):
     def __new__(cls, array=None):
         image_shape = array[0][1].shape if array is not None else (1, 1, 1)
-        dtype = np.dtype([('timestamp', '<i8'), ('image','i2', image_shape)])
+        dtype = np.dtype([('timestamp', '<i8'), ('image', 'i2', image_shape)])
         # dtype = np.dtype([('ts', '<M8[us]'), ('image','i2', list_of_tuple[0][1].shape)])
         if array is None:
             structured_array = np.array([], dtype)
@@ -17,8 +17,9 @@ class Frame(np.recarray):
     def find_closest(self, t):
         # idx = np.argmin(np.abs(self['ts'] - t)).astype(int)
         idx = np.searchsorted(self.timestamp, t)
-        if idx >= len(self): idx = len(self) - 1
-        return self.timestamp[idx], self.image[idx]
+        if idx >= len(self):
+            idx = len(self) - 1
+        return idx
 
     def __repr__(self) -> str:
         return str(self.dtype)
--- src/evtool/dvs/DvsFile.py
+++ src/evtool/dvs/DvsFile.py
@@ -4,6 +4,7 @@ import os.path as osp
 
 from dv import AedatFile
 import numpy.lib.recfunctions as rfn
+from scipy.io import savemat
 
 from evtool.utils.func import to_unit_frame
 from evtool.dtype import Event, Frame, Size, Data
@@ -35,13 +36,15 @@ class Load:
 
             # events
             if 'events' in f.names:
-                array_ev = np.hstack([packet for packet in f['events'].numpy()])
+                array_ev = np.hstack(
+                    [packet for packet in f['events'].numpy()])
                 array_ev = rfn.structured_to_unstructured(array_ev)[..., :4]
                 data['events'] = Event(array_ev)
 
             # frames
             if 'frames' in f.names:
-                array_fr = [(frame.timestamp, to_unit_frame(frame.image)) for frame in f['frames']]
+                array_fr = [(frame.timestamp, to_unit_frame(frame.image))
+                            for frame in f['frames']]
                 data['frames'] = Frame(array_fr)
 
         return data
@@ -56,9 +59,6 @@ class Load:
             array_ev = pd.read_csv(f, sep='\s+', header=None).values
             data['events'] = Event(array_ev)
 
-            # frames
-            data["frames"] = Frame()
-
         return data
 
 
@@ -71,6 +71,8 @@ class Save:
             pass
         elif extention == '.txt':
             return Save.to_txt(data, path)
+        elif extention == '.mat':
+            return Save.to_mat(data, path)
 
     @staticmethod
     def to_txt(data: Data, path):
@@ -80,11 +82,31 @@ class Save:
             np.savetxt(f, rfn.structured_to_unstructured(data['events']),
                        fmt='%16d %3d %3d %1d', delimiter=' ', newline='\n')
 
+    @staticmethod
+    def to_mat(data: Data, path):
+        class Struct:
+            pass
+        events = Struct()
+        frames = Struct()
+
+        events.timestamp = data['events'].timestamp
+        events.x = data['events'].x
+        events.y = data['events'].y
+        events.polarity = data['events'].polarity
+
+        if 'frames' in data.keys():
+            frames.timestamp = data['frames'].timestamp
+            frames.image = data['frames'].image
+
+        savemat(path, {'events': events, 'frames': frames,
+                'sizeX': data['size'][0], 'sizeY': data['size'][1]})
+
 
 class DvsFile:
     @staticmethod
     def load(path) -> Data:
         return Load.from_file(path)
 
-    def save(self) -> Data:
-        pass
+    @staticmethod
+    def save(data: Data, path) -> Data:
+        Save.to_file(data, path)
